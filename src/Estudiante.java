b849eb1fc53d42a93d7d7dd1b94b1b1a9c736942
Santiago <smartinez@cwti.com.co>
2022-05-24 20:50:52 -0500

    Constructor Pregunta 9

---
+++ src/Estudiante.java
@@ -0,0 +1,14 @@
+
+public class Estudiante {
+	
+	public String Nombre;
+	public Number CodigoNumerico;
+	
+	public Estudiante(String Nombre, Number CodigoNumerico) {
+
+		this.Nombre= Nombre;
+		this.CodigoNumerico=CodigoNumerico;
+				
+	}
+
+}
